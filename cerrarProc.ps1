8ac9fca7f338422cbca2259e60bb564fa0d94197
1DLW1 <88008917+1DLW1@users.noreply.github.com>
2023-10-13 03:17:15 +0100

    Add files via upload

---
+++ cerrarProc.ps1
@@ -0,0 +1,14 @@
+# Nombre del script a cerrar
+$proc = "opera.exe"  # Cambia esto al nombre de tu script
+
+# Buscar procesos que ejecutan el script
+$procesos = Get-Process | Where-Object { $_.MainModule.FileName -like "*\$proc" }
+
+# Verificar si se encontraron procesos
+if ($procesos.Count -gt 0) {
+    # Detener los procesos encontrados
+    $procesos | ForEach-Object { Stop-Process -Id $_.Id -Force }
+    Write-Host "Se cerraron correctamente los procesos que ejecutaban $proc"
+} else {
+    Write-Host "No se encontraron procesos que ejecutaran $proc"
+}
